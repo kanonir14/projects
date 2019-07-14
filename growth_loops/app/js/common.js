ff4a7f1ca31354412ed300ee9ea41ddb9b3e88c8
kanonir14 <kanonir1886@gmail.com>
2019-07-14 20:19:21 +0300

    desktop verison + mobile for main page

--- growth_loops/app/js/common.js
+++ growth_loops/app/js/common.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {
 
+	var w = $(window).width();
+
 	$(".sub-menu").closest('li').addClass('drop-menu');
+
+	if ( w <= 768 ) {
+		$(".drop-menu").click(function(event) {
+			$(".sub-menu").toggleClass('js-menu-show');
+			$(this).toggleClass('js-rotation');
+		});
+
+		$(".toggle-nav").click(function(event) {
+			$(".navigation").toggle();
+		});
+	}
 
 	// contact form
 
-	var mark = $(".contact-mark");
+	var mark = $(".contact-mark"),
+		input = $(".contact input");
 
 	mark.click(function(event) {
 		$(this).toggleClass('js-enable-mark');
 	});
-
-	// var label = $(this).closest('.form-group').find('label'),
-	// 	input = $(".contact-form input");
-
-	// input.focusin(function(event) {
-	// 	$(this).closest('.form-group').find('label').css('fontSize', '0');
-	// });
-	
-	// input.focusout(function(event) {
-	// 	$(this).closest('.form-group').find('label').css('fontSize', '21px');
-	// });
 
 
 });
